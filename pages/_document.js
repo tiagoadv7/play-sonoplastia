7d572a0cdfbd5e3e6059f1dc33186c9c3f34d3b0
Tiago Lima <51231966+tiagoadv7@users.noreply.github.com>
2024-12-07 23:51:30 -0300

    ajuste para o seo card img

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='pt-BR'>
-      <Head />
-      <body className='bg-gray-900'>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <Html lang="pt-BR">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
         <meta
           name="description"
@@ -16,22 +15,29 @@ export default function Document() {
           content="Sonoplastia, Player Sonoplastia, IASD, ferramentas para simplificar a sonoplastia"
         />
         <meta name="author" content="Tiago Lima" />
-
-        
         <title>=::Player Sonoplastia::=</title>
-
-       
         <link rel="icon" href="/favicon.svg" />
-        
-       
+
+        {/* Open Graph */}
         <meta property="og:title" content="=::Player Sonoplastia::=" />
-        <meta property="og:description" content="Site para facilitar a sonoplastia." />
+        <meta
+          property="og:description"
+          content="Simplifique sua rotina de sonoplastia com nossa plataforma eficiente e acessível."
+        />
         <meta
           property="og:image"
-          content="https://player-sonoplastia.vercel.app/favicon.svg"
+          content="https://github.com/tiagoadv7/play-sonoplastia/blob/main/public/static/images/seo/logo.png"
         />
         <meta property="og:url" content="https://player-sonoplastia.vercel.app" />
         <meta property="og:type" content="website" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Additional meta tags */}
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body className="bg-gray-900">
         <Main />
         <NextScript />
       </body>
