47203ba0a66d1c795ed63d4274cc3d3d50c5e3ef
Tiago Lima <51231966+tiagoadv7@users.noreply.github.com>
2024-12-07 23:43:28 -0300

    ajuste para seo

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,14 @@ export default function Document() {
       <body className='bg-gray-900'>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
-        <meta name="description" content="Sonoplastia IASD" />
-        <meta name="keywords" content="Sonoplastia IASD, Player Sonoplastia, para, facilitar no seu dia a dia" />
+        <meta
+          name="description"
+          content="Player Sonoplastia IASD - Simplifique sua rotina de sonoplastia com nossa plataforma eficiente e acessível."
+        />
+        <meta
+          name="keywords"
+          content="Sonoplastia, Player Sonoplastia, IASD, ferramentas para simplificar a sonoplastia"
+        />
         <meta name="author" content="Tiago Lima" />
 
         
@@ -20,7 +26,10 @@ export default function Document() {
        
         <meta property="og:title" content="=::Player Sonoplastia::=" />
         <meta property="og:description" content="Site para facilitar a sonoplastia." />
-        <meta property="og:image" content="https://github.com/tiagoadv7/play-sonoplastia/blob/main/public/static/logo/logo.webp" />
+        <meta
+          property="og:image"
+          content="https://player-sonoplastia.vercel.app/favicon.svg"
+        />
         <meta property="og:url" content="https://player-sonoplastia.vercel.app" />
         <meta property="og:type" content="website" />
 
