b29016eae4912ec1f2dc563157568fefe17bce2b
Tiago Lima <51231966+tiagoadv7@users.noreply.github.com>
2024-12-13 19:09:53 -0300

    ajustes imagens

--- components/ui/Hero/Hero.jsx
+++ components/ui/Hero/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => (
 </div>
 
                     </div>
-                    <GradientWrapper className="mt-16 sm:mt-28" wrapperClassName="max-w-3xl h-[250px] top-12 inset-0 sm:h-[300px] lg:h-[650px]">
+                    <GradientWrapper className="mt-16 sm:mt-28 flex items-center justify-center" wrapperClassName="max-w-3xl h-[250px] top-12 inset-0 sm:h-[300px] lg:h-[650px]">
                         <Image
                             src={HeroImg}
                             className="rounded-sm boxshadow-lg"
